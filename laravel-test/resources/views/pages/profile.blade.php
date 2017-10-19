3551acf96a8ae6a97b52336a453d6ab6e493e4d9
Sheldon Lynn <sheldonlynn@outlook.com>
2017-10-19 16:19:53 -0700

    templates are working

---
+++ laravel-test/resources/views/pages/profile.blade.php
@@ -0,0 +1,8 @@
+@extends('pages.master')
+
+@section('title', 'Company Profile')
+
+@section('content')
+    Company Profile
+
+@stop
--- laravel-test/routes/web.php
+++ laravel-test/routes/web.php
@@ -24,3 +24,5 @@ Route::get('contact', 'PagesController@contact');
 Route::get('about', 'PagesController@about');
 
 Route::get('posting/{id}', 'PostingsController@show');
+
+Route::resource('profile', 'CompanyProfileController');
